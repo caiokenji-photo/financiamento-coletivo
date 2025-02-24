b32e0b35ddb175058a9965bd2faa7e4e4212f2af
Luisa Burini <lburini@unicamp.br>
2025-02-23 21:38:39 -0300

    :beers: teste escrita tabela dos resultados

--- spreadsheet.js
+++ spreadsheet.js
@@ -100,21 +100,139 @@ function sortear() {
 }
 
 function salvarSorteado(sorteado) {
-  let access_token = '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'
   var params = {
     "key": access_token,
     "range":"Sorteio!A1",
     "majorDimension": "ROWS",
     "values": [
-    [sorteado]
-   ],
+      [sorteado]
+    ],
+  }
+  var xhr = new XMLHttpRequest();
+  xhr.open('PUT', 'https://sheets.googleapis.com/v4/spreadsheets/{' + sheetId+ '}/values/Sorteio!A1?valueInputOption=USER_ENTERED');
+  xhr.setRequestHeader('Authorization', 'Bearer ' + access_token);
+  xhr.send(JSON.stringify(params));
+  
 }
-var xhr = new XMLHttpRequest();
-xhr.open('PUT', 'https://sheets.googleapis.com/v4/spreadsheets/{' + sheetId+ '}/values/Sorteio!A1?valueInputOption=USER_ENTERED');
-xhr.setRequestHeader('Authorization', 'Bearer ' + access_token);
-xhr.send(JSON.stringify(params));
 
-}
+// Discovery doc URL for APIs used by the quickstart
+const CLIENT_ID = '78429735597-9gc1oejj42ofa33ev5qbnk081mmadm15.apps.googleusercontent.com ';
+const API_KEY = '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';
+const DISCOVERY_DOC = 'https://sheets.googleapis.com/$discovery/rest?version=v4';
+
+// Authorization scopes required by the API; multiple scopes can be
+// included, separated by spaces.
+const SCOPES = 'https://www.googleapis.com/auth/spreadsheets.readonly';
+
+let tokenClient;
+      let gapiInited = false;
+      let gisInited = false;
+
+      document.getElementById('authorize_button').style.visibility = 'hidden';
+      document.getElementById('signout_button').style.visibility = 'hidden';
+
+      /**
+       * Callback after api.js is loaded.
+       */
+      function gapiLoaded() {
+        gapi.load('client', initializeGapiClient);
+      }
+
+      /**
+       * Callback after the API client is loaded. Loads the
+       * discovery doc to initialize the API.
+       */
+      async function initializeGapiClient() {
+        await gapi.client.init({
+          apiKey: API_KEY,
+          discoveryDocs: [DISCOVERY_DOC],
+        });
+        gapiInited = true;
+        maybeEnableButtons();
+      }
+
+      /**
+       * Callback after Google Identity Services are loaded.
+       */
+      function gisLoaded() {
+        tokenClient = google.accounts.oauth2.initTokenClient({
+          client_id: CLIENT_ID,
+          scope: SCOPES,
+          callback: '', // defined later
+        });
+        gisInited = true;
+        maybeEnableButtons();
+      }
+
+      /**
+       * Enables user interaction after all libraries are loaded.
+       */
+      function maybeEnableButtons() {
+        if (gapiInited && gisInited) {
+          document.getElementById('authorize_button').style.visibility = 'visible';
+        }
+      }
+
+      /**
+       *  Sign in the user upon button click.
+       */
+      function handleAuthClick() {
+        tokenClient.callback = async (resp) => {
+          if (resp.error !== undefined) {
+            throw (resp);
+          }
+          document.getElementById('signout_button').style.visibility = 'visible';
+          document.getElementById('authorize_button').innerText = 'Refresh';
+          await listMajors();
+        };
+
+        if (gapi.client.getToken() === null) {
+          // Prompt the user to select a Google Account and ask for consent to share their data
+          // when establishing a new session.
+          tokenClient.requestAccessToken({prompt: 'consent'});
+        } else {
+          // Skip display of account chooser and consent dialog for an existing session.
+          tokenClient.requestAccessToken({prompt: ''});
+        }
+      }
+
+      /**
+       *  Sign out the user upon button click.
+       */
+      function handleSignoutClick() {
+        const token = gapi.client.getToken();
+        if (token !== null) {
+          google.accounts.oauth2.revoke(token.access_token);
+          gapi.client.setToken('');
+          document.getElementById('content').innerText = '';
+          document.getElementById('authorize_button').innerText = 'Authorize';
+          document.getElementById('signout_button').style.visibility = 'hidden';
+        }
+      }
+
+      /**
+       */
+      async function listMajors() {
+        let response;
+        try {
+          // Fetch first 10 files
+          response = await gapi.client.sheets.spreadsheets.values.get({
+            spreadsheetId: sheetId,
+            range: 'Sorteio!A1',
+          });
+        } catch (err) {
+          document.getElementById('content').innerText = err.message;
+          return;
+        }
+        const range = response.result;
+        if (!range || !range.values || range.values.length == 0) {
+          document.getElementById('content').innerText = 'No values found.';
+          return;
+        }
+        // Flatten to string to display
+        const output = range.values
+        document.getElementById('content').innerText = output;
+      }
     
 
 const userOs = document.querySelector(".os");
